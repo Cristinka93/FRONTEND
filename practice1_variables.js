9f3e5eb30f9fb2315aaeaeca418d9ef3fa5e8e4e
CL <yourOwn@email.comcristinabotnari@yahoo.com>
2023-05-18 11:58:55 -0500

    if else java screept

---
+++ practice1_variables.js
@@ -0,0 +1,32 @@
+console.log("Hello World");
+console.log('hello from single quote');
+
+// 3 ways we can define variable
+// var, let
+// const if the variable value is fixed and cannot be changed
+
+
+
+let  message = "Hello";
+
+console.log(message);
+var num = 100;
+console.log(num);
+
+
+let price = 1399.99;
+
+console.log('Iphone price is '+price);
+
+let hired = true;
+console.log('Cristina is hiered? ' + hired);
+let coffee;
+console.log(coffee);
+
+const name = "Cavidan";
+
+name = "something else";
+
+
+
+
